321f999dd22b3e69db6518eb830f234351e035b9
lvzh0414 <112322253+lvzh0414@users.noreply.github.com>
2023-01-07 20:43:51 +0800

    Initial commit

---
+++ src/main/java/com/lvzh/micronaut/guide/Application.java
@@ -0,0 +1,10 @@
+package com.lvzh.micronaut.guide;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
